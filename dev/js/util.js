5b1e460c3238863436f2e934f8c2ca99a2e785e3
水野 和弥 <kazu8160@gmail.com>
2015-09-21 19:37:36 +0900

    update

--- dev/js/util.js
+++ dev/js/util.js
@@ -1,21 +1,11 @@
 'use strict';
 
-<<<<<<< HEAD
-import request  from 'superagent';
-import reqJsonp from 'superagent-jsonp';
-
-reqJsonp(request);
-
-=======
-'use strict';
-
 import request  from 'superagent';
 import reqJsonp from 'superagent-jsonp';
 import { ORDERBY_ASC, ORDERBY_DESC } from './variables';
 
 reqJsonp(request);
 
->>>>>>> gh-pages
 export default {
     /**
      * dataを並び替えて新しい配列を生成する
@@ -33,18 +23,6 @@ export default {
         });
     },
 
-<<<<<<< HEAD
-    fetch( url ) {
-        let defer = Promise.defer();
-        request.get( url ).jsonp().end( ( err, res ) => {
-            if ( res ) {
-                defer.resolve( res.body );
-            } else {
-                defer.reject( err );
-            }
-        });
-        return defer.promise;
-=======
     /**
      * ajaxでデータの取得
      * @param  {String} url
@@ -61,6 +39,5 @@ export default {
                 }
             });
         });
->>>>>>> gh-pages
     }
 }
