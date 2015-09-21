ce082d487f9aafc910e933079f60df2e4c9d6391
水野 和弥 <kazu8160@gmail.com>
2015-09-21 17:08:32 +0900

    update

--- dev/js/SearchForm.jsx
+++ dev/js/SearchForm.jsx
@@ -1,3 +1,5 @@
+'use strict';
+
 import React from 'react';
 
 export default class SearchForm extends React.Component {
--- dev/js/util.js
+++ dev/js/util.js
@@ -1,7 +1,10 @@
 'use strict';
 
+'use strict';
+
 import request  from 'superagent';
 import reqJsonp from 'superagent-jsonp';
+import { ORDERBY_ASC, ORDERBY_DESC } from './variables';
 
 reqJsonp(request);
 
@@ -10,26 +13,33 @@ export default {
      * dataを並び替えて新しい配列を生成する
      * @param  {Object} data
      * @param  {String} order
+     * @param  {String} key
      * @return {Array}
      */
     sortBydata( data, order, key ) {
         return Array.prototype.slice.call( data ).sort( ( a, b ) => {
             switch ( order ) {
-                case 'asc'  : return ( a[ key ] < b[ key ] ) ? -1 : 1;
-                case 'desc' : return ( a[ key ] > b[ key ] ) ? -1 : 1;
+                case ORDERBY_ASC  : return ( a[ key ] < b[ key ] ) ? -1 : 1;
+                case ORDERBY_DESC : return ( a[ key ] > b[ key ] ) ? -1 : 1;
             }
         });
     },
 
+    /**
+     * ajaxでデータの取得
+     * @param  {String} url
+     * @return {Object} promise
+     */
     fetch( url ) {
-        let defer = Promise.defer();
-        request.get( url ).jsonp().end( ( err, res ) => {
-            if ( res ) {
-                defer.resolve( res.body );
-            } else {
-                defer.reject( err );
-            }
+        // Promise.defer()はFirefoxでは未実装？
+        return new Promise( ( resolve, reject ) => {
+            request.get( url ).jsonp().end( ( err, res ) => {
+                if ( res ) {
+                    resolve( res.body );
+                } else {
+                    reject( err );
+                }
+            });
         });
-        return defer.promise;
     }
 }
